f85c5b8268d2061bc56f0100c23abf9c73c436f9
hyunju <hjko_o@daycore.co.kr>
2017-06-14 16:49:10 +0900

    helloWorld 수정

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="helloworld">
+  <div class="hello-world">
     <div class="container">
       <div class="row align-items-center fullheight bubble-bg">
         <div class="col">
@@ -23,16 +23,19 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .helloworld {
+  .hello-world {
     background: #f0ffff;
   }
+
   .fullheight {
     min-height: 100vh;
   }
+
   .bubble-bg {
     position: relative;
     overflow: hidden;
   }
+
   .bubble {
     position: absolute;
     bottom: -50px;
@@ -41,26 +44,32 @@
     border-radius: 50%;
     background-color: #000000;
   }
+
   .bubble:nth-of-type(1) {
     left: 10px;
     animation: bubble 10s 2s linear infinite;
   }
+
   .bubble:nth-of-type(2) {
     left: 50%;
     animation: bubble 10s linear infinite;
   }
+
   .bubble:nth-of-type(3) {
     left: 100px;
     animation: bubble 8s linear infinite;
   }
+
   .bubble:nth-of-type(4) {
     right: 100px;
     animation: bubble 5s linear infinite;
   }
+
   .bubble:nth-of-type(5) {
     right: 10px;
     animation: bubble 15s linear infinite;
   }
+
   @keyframes bubble {
     0% {
       bottom: -100px;
@@ -74,6 +83,7 @@
       opacity: 0;
     }
   }
+
   h1 {
     margin-bottom: 50px;
     font-size: 45pt;
