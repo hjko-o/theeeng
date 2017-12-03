908e29d0fdd9d12b24617197880de2c832f10741
hjko_o <hjko_o@daycore.co.kr>
2017-12-03 18:16:30 +0900

    link -> to 속성으로 변경

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
     <b-navbar toggleable toggle-breakpoint="sm" fixed="top" type="light" variant="faded">
-      <b-navbar-brand tag="h1" class="mb-0" href="/#">
+      <b-navbar-brand tag="h1" class="mb-0" to="/">
         <img class="logo" src="../static/assets/theeeng_logo.png"/>
       </b-navbar-brand>
       <b-nav-toggle target="nav_dropdown_collapse" href="#"></b-nav-toggle>
 
       <b-collapse is-nav id="nav_dropdown_collapse">
         <b-nav is-nav-bar>
-          <b-nav-item href="#/portfolio">Portfolio</b-nav-item>
-          <b-nav-item href="#">메뉴1</b-nav-item>
+          <b-nav-item to="/portfolio">Portfolio</b-nav-item>
+          <b-nav-item to="/portfolio">메뉴1</b-nav-item>
           <b-nav-item href="http://instagram.com/hjko_o">Instagram</b-nav-item>
           <b-nav-item href="http://blog.naver.com/hjko_o">Blog</b-nav-item>
         </b-nav>
