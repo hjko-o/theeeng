814d6bca83c9be2d9bf55b99a67db4ed84a2c44e
hjko_o <hjko_o@daycore.co.kr>
2017-12-03 18:31:24 +0900

    스타일 수정

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,6 +23,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
   .hello-world {
     background: #f0ffff;
   }
--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -9,12 +9,6 @@
     </div>
     <div class="portfolio-career">
       <div class="career">
-        <h2>솔게이트 (15.06~16.04)</h2>
-        <h3>레몬커플(Remoncouple) 홈페이지 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
-        <h3>레몬커플 관리자 페이지 디버깅<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
-        <h3>소개팅 홈페이지 기획 및 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
-      </div>
-      <div class="career">
         <h2>데이코어 (16.06~17.12)</h2>
         <h3>랩짐데이 홈페이지 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
         <h3>랩짐데이 트레이너 관리용 웹 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
@@ -22,6 +16,12 @@
         <h3>데이코어 회사 홈페이지 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
         <h3>랩짐데이 그룹피티 관리용 웹 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
         <h3>짐데이 ios 앱 업데이트 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
+      </div>
+      <div class="career">
+        <h2>솔게이트 (15.06~16.04)</h2>
+        <h3>레몬커플(Remoncouple) 홈페이지 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
+        <h3>레몬커플 관리자 페이지 디버깅<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
+        <h3>소개팅 홈페이지 기획 및 개발<span><icon class="arrow-icon" name="chevron-circle-down" scale="1"></icon></span></h3>
       </div>
     </div>
   </div>
@@ -85,7 +85,6 @@
     color: #333;
     text-align: left;
     cursor: pointer;
-    /*background-color: rgba(255, 255, 255, 0.5);*/
   }
 
   .portfolio-career h3:hover {
